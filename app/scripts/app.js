6880173952a311932fdec684cd1f27576e929d6e
Max Igor <maxigor.ferreira@gmail.com>
2016-02-23 12:42:53 -0300

    i hate this fucking job

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -805,6 +805,7 @@ var adiciona = document.getElementById('add');
 var texto = document.getElementById('texto');
 var tabela = document.getElementById('tabela');
 
+
 adiciona.addEventListener('click', function(){
       tabela.appendChild('<tr><td>'+ texto.value +'<a class="button is-danger is-outlined"><i class="fa fa-times"></i>Delete</a></td></tr>')
       texto.value = '';
