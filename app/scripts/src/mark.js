643134d26e816a75dad09ebe57bc50be6ff794b9
Max Igor <maxigor.ferreira@gmail.com>
2016-02-24 03:17:37 -0300

    this doesn't really make things faster, but I tried

--- app/scripts/src/mark.js
+++ app/scripts/src/mark.js
@@ -2,7 +2,7 @@ var add = document.querySelector("#add");
 var remove = document.querySelector("#delete");
 
 add.addEventListener('click', function(){
-   var novoTodo = "<tr> " +
+   var novoTodo = "<tr class='wait'> " +
                      "<td>" + texto.value +
                         "<a id='delete' class='button is-danger is-outlined'>" +
                            "<i class='remove fa fa-times'></i>"+
@@ -14,4 +14,8 @@ add.addEventListener('click', function(){
    tabela.innerHTML = tabela.innerHTML + novoTodo;
 
    texto.value = '';
-})
+});
+
+remove.addEventListener('click',function(){
+   this.remove('.wait');
+});
